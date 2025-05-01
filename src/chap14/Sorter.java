17369236939f66e14a3f13477eec34fc72e3a9e4
aarogill23 <angill23@live.com>
2025-04-30 23:00:30 -0500

    finished sorter assignment

--- src/chap14/Sorter.java
+++ src/chap14/Sorter.java
@@ -3,26 +3,29 @@ package chap14;
 import java.util.ArrayList;
 
 public class Sorter {
-    
+
     /**
      * Sorts the given array in ascending order "in place".
-     * @param unsorted - the original array which will be modified by the sort algorithm
+     * 
+     * @param unsorted - the original array which will be modified by the sort
+     *                 algorithm
      */
     public static void selectionSort(int[] unsorted) {
-        for (int i=0; i<unsorted.length-1; i++) {
+        for (int i = 0; i < unsorted.length - 1; i++) {
             // int current = unsorted[i];
-            int minrest = findMin(i+1, unsorted);
-            if (unsorted[minrest]<unsorted[i])
+            int minrest = findMin(i + 1, unsorted);
+            if (unsorted[minrest] < unsorted[i])
                 swap(i, minrest, unsorted);
         }
 
     }
+
     // returns the index of the minimum (not the value)
     public static int findMin(int start, int array[]) {
         int themin = array[start];
         int thewinner = start;
-        for (int i=start+1; i<array.length; i++) {
-            if (array[i]<themin) {
+        for (int i = start + 1; i < array.length; i++) {
+            if (array[i] < themin) {
                 thewinner = i;
                 themin = array[i];
             }
@@ -37,10 +40,10 @@ public class Sorter {
     }
 
     public static void bubbleSort(int array[]) {
-        for (int i=0; i<array.length; i++) {
-            for (int j=0; j<array.length-i-1; j++) {
-                if (array[j]>array[j+1]) {
-                    swap(j, j+1, array);
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    swap(j, j + 1, array);
                 }
             }
         }
@@ -53,16 +56,16 @@ public class Sorter {
      * @return the sorted list
      */
     public static <T extends Comparable<? super T>> ArrayList<T> mergeSort(ArrayList<T> list) {
-        if (list.size()<=1)
+        if (list.size() <= 1)
             return list;
-        
+
         // Recursive case. First, divide the list into equal-sized sublists
         // consisting of the first half and second half of the list.
         // This assumes lists start at index 0.
         ArrayList<T> left = new ArrayList<>();
         ArrayList<T> right = new ArrayList<>();
-        for (int i=0; i<list.size(); i++) {
-            if (i<list.size()/2) {
+        for (int i = 0; i < list.size(); i++) {
+            if (i < list.size() / 2) {
                 left.add(list.get(i));
             } else {
                 right.add(list.get(i));
@@ -77,52 +80,52 @@ public class Sorter {
         return merge(left, right);
     }
 
+    @SuppressWarnings("unchecked")
     public static <T extends Comparable<? super T>> ArrayList<T> merge(ArrayList<T> left, ArrayList<T> right) {
+
         // Implement this pseudocode to finish mergesort:
         /*
-            var result := empty list
-            while left is not empty and right is not empty do
-                if first(left) ≤ first(right) then ... instead of using <=, use compareTo method and check if return value <= 0 otherwise it must have been greater than ... see below:                
-                if (first(left).compareTo(first(right)) <= 0) then
-                    append first(left) to result
-                    left := rest(left)
-                else
-                    append first(right) to result
-                    right := rest(right)
-
-            // Either left or right may have elements left; consume them.
-            // (Only one of the following loops will actually be entered.)
-            while left is not empty do
-                append first(left) to result
-                left := rest(left)
-            while right is not empty do
-                append first(right) to result
-                right := rest(right)
-            return result
+         * var result := empty list
+         * while left is not empty and right is not empty do
+         * if first(left) ≤ first(right) then ... instead of using <=, use compareTo
+         * method and check if return value <= 0 otherwise it must have been greater
+         * than ... see below:
+         * if (first(left).compareTo(first(right)) <= 0) then
+         * append first(left) to result
+         * left := rest(left)
+         * else
+         * append first(right) to result
+         * right := rest(right)
+         * 
+         * // Either left or right may have elements left; consume them.
+         * // (Only one of the following loops will actually be entered.)
+         * while left is not empty do
+         * append first(left) to result
+         * left := rest(left)
+         * while right is not empty do
+         * append first(right) to result
+         * right := rest(right)
+         * return result
          */
-
-        ArrayList<T> result = new ArrayList<>();
+        @SuppressWarnings("rawtypes")
+        ArrayList result = new ArrayList<T>();
         while (!left.isEmpty() && !right.isEmpty()) {
-            if (left.getFirst().compareTo(right.getFirst()) <= 0) {
-                result.add(left.getFirst());
+            if (left.get(0).compareTo(right.get(0)) <= 0) {
+                result.add(left.get(0));
                 left.remove(0);
             } else {
-                result.add(right.getFirst());
+                result.add(right.get(0));
                 right.remove(0);
             }
         }
-        // Either left or right may have elements left; consume them.
-        // (Only one of the following loops will actually be entered.)
         while (!left.isEmpty()) {
-            result.add(left.remove(0));
+            result.add(left.get(0));
+            left.remove(0);
         }
         while (!right.isEmpty()) {
-            result.add(right.remove(0));
+            result.add(right.get(0));
+            right.remove(0);
         }
-        // return the merged list
         return result;
-
     }
-
-
 }
